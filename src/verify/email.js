08ac7bd16dfb74caf382d8aff692a79a07e3111c
Claudio4 <me@claudio4.com>
2017-10-09 17:44:04 +0100

    Bug fix

--- src/verify/email.js
+++ src/verify/email.js
@@ -1,5 +1,5 @@
 const RegEx = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 module.exports = function(email) {
-  return (!!email && email.match(RegEx));
+  return (!!email && !!email.match(RegEx));
 }
